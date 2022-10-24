3dcd7ea966325ac6eb4682609e68a2a11d06cd65
gdudik <68257224+gdudik@users.noreply.github.com>
2022-10-24 11:03:58 -0700

    move to two commands and eval expressions first

--- cwremotecontrol.js
+++ cwremotecontrol.js
@@ -10,18 +10,31 @@ let localLynxServer = net.createServer((socket) => {
       
       const axios = require('axios');
 
-      const playmotion = [ { 
-      "action": "play_motions",
-     "motions": "TimeResults Competitor Data",
-    },
+      const eval_expressions =  
     {
         "action": "activate_grid_cell",
         "grid": "grid",
         "cell": [0,0],
-      } ];
+      } ;
+
+      const play_results =  { 
+        "action": "play_motions",
+       "motions": "TimeResults Competitor Data",
+      } ;
 
 
-  axios.post('http://192.168.1.44:5201', playmotion)
+  axios.post('http://192.168.1.44:5201', eval_expressions)
+    .then((res) => {
+      console.log(`Status: ${res.status}`);
+      console.log(res.data);
+    }).catch((err) => {
+    console.error(err);
+    });
+      
+      resultsActive = true;
+      timeActive = false;
+
+  axios.post('http://192.168.1.44:5201', play_results)
     .then((res) => {
       console.log(`Status: ${res.status}`);
       console.log(res.data);
@@ -32,6 +45,7 @@ let localLynxServer = net.createServer((socket) => {
       resultsActive = true;
       timeActive = false;
     }
+    
     
     if (lynxData.indexOf('Command=LayoutDraw;Name=Time;Clear=1;') !== -1) {
       const axios = require('axios');
