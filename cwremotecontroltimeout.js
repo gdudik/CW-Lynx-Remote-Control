aa323a8264175dd9b1eb12a5879fbccd04631952
Graham Dudik <graham.dudik@athletic.net>
2023-02-20 16:28:48 -0800

    pvp and dak cw signals

--- cwremotecontroltimeout.js
+++ cwremotecontroltimeout.js
@@ -5,6 +5,24 @@ let localLynxServer = net.createServer((socket) => {
   socket.on("data", (async (data) => {
     let lynxData = data.toString();
     console.log(lynxData)
+//If starts signal sent from lynx, trigger the dak to the starts list button    
+    
+    if (lynxData.indexOf('Command=LayoutDraw;Name=Starts;Window=2;Clear=1;') !== -1 && resultsActive === false) {
+      const axios = require('axios');
+      const dak_starts = {
+        "action": "activate_grid_cell",
+        "grid": "grid",
+        "cell": [6, 1],
+      };
+      axios.post('http://192.168.1.44:5201', dak_starts)
+        .then((res) => {
+          console.log(`Status: ${res.status}`);
+          console.log(res.data);
+        }).catch((err) => {
+          console.error(err);
+        });
+    }
+// If results signal sent from lynx, evaluate the expressions by triggering the grid button, wait 1s, play the motion on the floor boards. Also clear the clock from PVP and open the results layout on the dak.
     if (lynxData.indexOf('Command=LayoutDraw;Name=Results;Window=2;Clear=1;') !== -1 && resultsActive === false) {
       const axios = require('axios');
       const eval_expressions =
@@ -17,6 +35,20 @@ let localLynxServer = net.createServer((socket) => {
         "action": "play_motions",
         "motions": "TimeResults Competitor Data",
       };
+      const dak_results = {
+        "action": "activate_grid_cell",
+        "grid": "grid",
+        "cell": [6, 0],
+      };
+      axios.post('http://192.168.1.44:5201', dak_results)
+        .then((res) => {
+          console.log(`Status: ${res.status}`);
+          console.log(res.data);
+        }).catch((err) => {
+          console.error(err);
+        });
+
+
       axios.post('http://192.168.1.50:49345/api/0/clear/layer/Clock-corners')
         .then((res) => {
           console.log(`Status: ${res.status}`);
@@ -53,6 +85,7 @@ let localLynxServer = net.createServer((socket) => {
       resultsActive = true;
       timeActive = false;
     }
+// If time signal received from lynx, play the running time motion in CW and then trigger the clock back in on both PVP layouts.
 
     if (lynxData.indexOf('Command=LayoutDraw;Name=Time;Clear=1;') !== -1) {
       const axios = require('axios');
